8edc39852eb6b66128a755af6e95c98f8e569445
ema <mccoy39082@163.com>
2024-11-18 14:06:13 +0800

    chore: ignore warning

---
+++ src/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+// Suppresses the warning CA1816: Dispose methods should call SuppressFinalize.
+// This is because the assembly does not contain unmanaged resources that require finalization.
+[assembly: SuppressMessage("Microsoft.Design", "CA1816:DisposeMethodsShouldCallSuppressFinalize", Justification = "No unmanaged resources in the assembly.")]
+
+// Suppresses the warning CA2012: Dispose methods should call SuppressFinalize.
+// This is because the assembly does not contain unmanaged resources that require finalization.
+[assembly: SuppressMessage("Microsoft.Design", "CA2012:DisposeMethodsShouldCallSuppressFinalize", Justification = "No unmanaged resources in the assembly.")]
